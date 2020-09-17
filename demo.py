e3036ce44d50be5a5af75c32890c0fa1e5ac0fb0
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-17 12:00:50 -0400

    Initial commit

---
+++ demo.py
@@ -0,0 +1,29 @@
+def main():
+    smiles = "\U0001F600 \n\U0001F600 \n\U0001F600"
+    print (smiles)
+
+
+def demo():
+    number = 4
+    print(f"the number is {number * 4}")
+    not_really_a_number = "123 "
+    print(f"but {not_really_a_number*4}")
+
+demo()
+main()
+
+def example():
+    response= input (f"how happy are you from 1-10")
+    howmany = int(response)
+    smiles = "\U0001F600" *howmany
+    print (smiles)
+
+
+
+
+
+
+
+
+
+example()
